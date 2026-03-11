a4137b35bf97cdbf9f9dba46e47e1ada1e41b037
giribabu <snadha927@gmail.com>
2026-03-11 13:03:27 +0530

    Changes done in OtherTE

--- src/test/java/practice/Demo.java
+++ src/test/java/practice/Demo.java
@@ -5,7 +5,7 @@ public class Demo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("main started");
-		System.out.println("-------");
+		System.out.println("Hello Guy");
 		System.out.println("main end");
 	}
 
