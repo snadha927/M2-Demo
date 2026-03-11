6c6433301423bc0026261c46a8013d4d0d38cbc7
giribabu <snadha927@gmail.com>
2026-03-11 11:23:21 +0530

    This is my 2nd commit

--- src/test/java/practice/Demo.java
+++ src/test/java/practice/Demo.java
@@ -5,7 +5,7 @@ public class Demo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hello Guys");
+		System.out.println("Hello Guy");
 
 	}
 
