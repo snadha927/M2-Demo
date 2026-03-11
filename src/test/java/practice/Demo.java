345f9d3d8bcf029ee1c857d2164bdf532ee70029
giribabu <snadha927@gmail.com>
2026-03-11 11:29:47 +0530

    changes done in demo

--- src/test/java/practice/Demo.java
+++ src/test/java/practice/Demo.java
@@ -4,9 +4,9 @@ public class Demo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		System.out.println("main started");
 		System.out.println("Hello Guy");
-
+		System.out.println("main end");
 	}
 
 }
