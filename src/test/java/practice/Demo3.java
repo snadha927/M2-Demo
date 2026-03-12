4688279cafb158567bd476bd44cd4507e7a6e465
giribabu <snadha927@gmail.com>
2026-03-12 12:47:05 +0530

    commit to branch

---
+++ src/test/java/practice/Demo3.java
@@ -0,0 +1,11 @@
+package practice;
+
+public class Demo3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("pushing from M2-Demo OtherTE to brach first1");
+
+	}
+
+}
